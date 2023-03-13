68f0c0b1b17ed56e5ed0aaa51cffd03029846c3f
dicko_bisabanget <>
2023-03-13 11:19:08 +0700

    init

---
+++ resources/views/index.blade.php
@@ -0,0 +1,374 @@
+<!DOCTYPE html>
+<html class="no-js" style="display: block;" lang="zxx"><head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="canonical" href="/">
+
+    <title>Kursus Stir Mobil</title>
+
+    <meta name="description" content="Kursus Stir Mobil">
+    <meta name="keywords" content="Kursus Stir Mobil">
+    <meta name="title" content="Kursus Stir Mobil">
+
+    <link href="{{ asset('css/css.css') }}" rel="stylesheet">
+    <!--
+    CSS
+    ============================================= -->
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/main.css')}}">
+</head>
+<body style="display: block;">
+<button type="button" id="mobile-nav-toggle"><i class="lnr lnr-menu"></i></button>
+<header id="header" class="header-scrolled">
+    <div class="header-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-sm-8 col-4 header-top-left no-padding">
+                    <ul>
+                        <li><a href="tel:+08xxx"><span class="lnr lnr-phone-handset"></span><span class="text">08x xxxxx xxxx</span></a></li>
+                        <li><a href="mailto:info@kursusmobil.test"><span class="lnr lnr-envelope"></span><span class="text">info@kursusmobil.test</span></a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-6 col-sm-4 col-8 header-top-right no-padding">
+                    <a class="text-uppercase top-head-btn" href="tel:+08xxx">Hubungi Sekarang</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container main-menu">
+        <div class="row align-items-center justify-content-between d-flex">
+            <div id="logo">
+            </div>
+            <nav id="nav-menu-container">
+                <ul class="nav-menu sf-js-enabled sf-arrows" style="touch-action: pan-y;">
+                    <li class="menu-active"><a href="/#beranda">Beranda</a></li>
+                    <li><a href="/#jenis-kursus">Kursus Terbaik</a></li>
+                    <li><a href="/#tentang-kami">Tentang Kami</a></li>
+                    <li><a href="/#kelebihan-kami">Kelebihan Kami</a></li>
+                    <li><a href="/#customer-service">Hubungi Kami</a></li>
+                </ul>
+            </nav><!-- #nav-menu-container -->
+        </div>
+    </div>
+</header><!-- #header -->
+<!-- start banner Area -->
+<section class="banner-area relative" id="beranda">
+    <div class="container">
+        <div class="row fullscreen d-flex align-items-center justify-content-start" style="height: 803px;">
+            <div class="banner-content col-lg-8 col-md-12">
+                <h1 class="text-uppercase">
+                    Kursus Mengemudi Mobil di Persemija<br>
+                    Pilihan Terpercaya <br>
+                    Untuk Kursus Mengemudi Anda
+                </h1>
+                <p class="text-white">
+                    Kursus mengemudi Pesona Jaya ini sudah berdiri sejak tahun 1997.<br>
+                </p>
+                <a target="_blank" href="https://api.whatsapp.com/send?phone=08xxx&amp;text=Halo" class="primary-btn header-btn text-uppercase">Pesan Sekarang</a>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End banner Area -->
+
+<!-- Start details Area -->
+<section class="details-area pt-50">
+    <div class="container">
+        <div class="details-section">
+            <div class="row align-items-center">
+                <div class="col-lg-2 detials-left">
+                    <h5 class="text-uppercase">promo spesial</h5>
+                    <p>
+                        Penawaran Terbatas
+                    </p>
+                </div>
+                <div class="col-lg-9 detials-right">
+                    <p style="font-size: 30px;">DISKON HINGGA 15% SELAMA PERIODE PROMO</p>
+                    <small>
+                        Syarat &amp; Ketentuan Berlaku
+                    </small>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End details Area -->
+
+<!-- Start price Area -->
+<section class="price-area section-gap" id="jenis-kursus">
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="menu-content pb-60 col-lg-8">
+                <div class="title text-center">
+                    <h1 class="mb-10">KURSUS TERBAIK UNTUK ANDA</h1>
+                    <p>Semua kursus didampingi oleh profesional.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="single-price">
+                    <div class="top-sec d-flex justify-content-between">
+                        <div class="top-left">
+                            <h4>Paket 8x1</h4>
+                            <p>8 kali pertemuan</p>
+                            <h1>Rp780.000</h1>
+                        </div>
+                    </div>
+                    <div class="bottom-sec">
+
+                    </div>
+                    <div class="end-sec">
+                        <ul>
+                            <li>Cara Belajar Mobil dengan kaidah Safety Driving</li>
+                            <li>Cara Mengendarai Mobil di Jalan Raya</li>
+                            <li>Cara Mengoper Gigi Mobil Manual & Automatic</li>
+                            <li>Cara Mengemudi Mobil Manual di Tanjakan</li>
+                            <li>Cara Mengemudi Mobil Mundur</li>
+                            <li>Cara Mengendarai Mobil di Belokan</li>
+                            <li>Cara Parkir Mobil Paralel dan Seri</li>
+                            <li>Tips & Trik Berkendara</li>
+                        </ul>
+                        <a target="_blank" rel="nofollow" href="https://api.whatsapp.com/send?phone=08xxx&amp;text=Selamat%20Pagi,%20Saya%20ingin%20bertanya%20tentang%20produk%20WIKA%20SWH%20TSC%20130">
+                            <button class="primary-btn price-btn mt-20">Pesan Sekarang<span class="lnr lnr-arrow-right"></span></button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="single-price">
+                    <div class="top-sec d-flex justify-content-between">
+                        <div class="top-left">
+                            <h4>Paket 12x1</h4>
+                            <p>12 kali pertemuan</p>
+                            <h1>Rp1.060.000</h1>
+                        </div>
+                    </div>
+                    <div class="bottom-sec">
+
+                    </div>
+                    <div class="end-sec">
+                        <ul>
+                            <li>Cara Belajar Mobil dengan kaidah Safety Driving</li>
+                            <li>Cara Mengendarai Mobil di Jalan Raya</li>
+                            <li>Cara Mengoper Gigi Mobil Manual & Automatic</li>
+                            <li>Cara Mengemudi Mobil Manual di Tanjakan</li>
+                            <li>Cara Mengemudi Mobil Mundur</li>
+                            <li>Cara Mengendarai Mobil di Belokan</li>
+                            <li>Cara Parkir Mobil Paralel dan Seri</li>
+                            <li>Tips & Trik Berkendara</li>
+                        </ul>
+                        <a target="_blank" rel="nofollow" href="https://api.whatsapp.com/send?phone=08xxx&amp;text=Selamat%20Pagi,%20Saya%20ingin%20bertanya%20tentang%20produk%20SOLAHART%20181%20L">
+                            <button class="primary-btn price-btn mt-20">Pesan Sekarang<span class="lnr lnr-arrow-right"></span></button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="single-price">
+                    <div class="top-sec d-flex justify-content-between">
+                        <div class="top-left">
+                            <h4>Paket 16x1</h4>
+                            <p>16 kali pertemuan</p>
+                            <h1>Rp1.350.000</h1>
+                        </div>
+                    </div>
+                    <div class="bottom-sec">
+
+                    </div>
+                    <div class="end-sec">
+                        <ul>
+                            <li>Cara Belajar Mobil dengan kaidah Safety Driving</li>
+                            <li>Cara Mengendarai Mobil di Jalan Raya</li>
+                            <li>Cara Mengoper Gigi Mobil Manual & Automatic</li>
+                            <li>Cara Mengemudi Mobil Manual di Tanjakan</li>
+                            <li>Cara Mengemudi Mobil Mundur</li>
+                            <li>Cara Mengendarai Mobil di Belokan</li>
+                            <li>Cara Parkir Mobil Paralel dan Seri</li>
+                            <li>Tips & Trik Berkendara</li>
+                        </ul>
+                        <a target="_blank" href="https://api.whatsapp.com/send?phone=08xxx&amp;text=Selamat%20Pagi,%20Saya%20ingin%20bertanya%20tentang%20produk%20SOLAR%20WATER%20HEATER%20HANDAL%20151%20PQ">
+                            <button class="primary-btn price-btn mt-20">Pesan Sekarang<span class="lnr lnr-arrow-right"></span></button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End price Area -->
+
+<!-- Start about Area -->
+<section class="about-area" id="tentang-kami">
+    <div class="container-fluid">
+        <div class="row d-flex justify-content-end align-items-center">
+            <div class="col-lg-6 col-md-12 about-left">
+                <h1>TENTANG KAMI</h1>
+                <p style="text-align: justify;">
+                    Persemija merupakan tempat kursus mengemudi mobil yang cukup terkenal di Jakarta. Tempat kursus mobil ini membuka beberapa cabang, seperti di Benhil, Gandaria, Radio Dalam, dan lainnya. Biaya kursus menyetir mobil paling murah seharga Rp530 ribu dan biaya paling mahal seharga Rp1,450 juta. Banyak keunggulan yang membuat Kursus Mengemudi Persemija berkembang cukup pesat dan menjadikan Kursus Mengemudi Persemija lembaga pelatihan mengemudi terpercaya dan telah meluluskan ribuan pengemudi handal dan santun. Dengan beberapa keunggulan yang dimiliki menjadikan siswa cepat mahir dan menguasai kendaraan. Keunggalan ini yang membuat Kursus Mengemudi Persemija berbeda dengan kursus mengemudi lainya.
+                </p>
+            </div>
+            <div class="col-lg-6 col-md-12 about-right no-padding">
+
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End about Area -->
+
+<!-- Start service Area -->
+<section class="service-area section-gap" id="kelebihan-kami">
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-8 pb-40 header-text">
+                <h1>KENAPA MEMILIH KAMI ?</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-6">
+                <div class="single-service">
+                    <h4><span class="lnr lnr-user"></span>Bebas BBM (Tidak Dijatah)</h4>
+                    <p>
+                        Berapapun habisnya BBM akan ditanggung oleh perusahaan, sehingga instruktur bisa lebih leluasa dalam mengajar
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="single-service">
+                    <h4><span class="lnr lnr-license"></span>Bebas Pilih Mobil</h4>
+                    <p>
+                        Siswa bebas memilih mobil karena Kursus Mengmudi “PRIVATE” menyediakan berbagai jenis mobil terbaru dari yang berukuran kecil sampai besar.
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="single-service">
+                    <h4><span class="lnr lnr-phone"></span>Jadwal belajar fleksibel</h4>
+                    <p>
+                        Siswa dapat menentukan jadwal sendiri dengan fleksibel. Sabtu / Minggu maupun hari besar tetap dilayani.
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="single-service">
+                    <h4><span class="lnr lnr-rocket"></span>KECEWA ?? GRATIS !!</h4>
+                    <p>
+                        Semua siswa mendapatkan jaminan pelayanan terbaik dari Kursus Mengemudi “PRIVATE”, apabila siswa merasa kecewa akan pelayanan saat proses belajar berlangsung, maka siswa berhak mendapatkan GRATIS 2x BELAJAR langsung tanpa ditanya Alasanya.
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="single-service">
+                    <h4><span class="lnr lnr-diamond"></span>Durasi Utuh</h4>
+                    <p>
+                        Kursus Mengemudi “PRIVATE” memberikan jaminan dan garansi bahwa durasi belajar akan utuh.
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="single-service">
+                    <h4><span class="lnr lnr-bubble"></span>Gratis Biaya Penjemputan</h4>
+                    <p>
+                        Salah satu keunggulan lainnya yakni penjemputan keseluruh kota dimana Kursus Mengemudi “PRIVATE” berada tanpa biaya tambahan.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End service Area -->
+
+<!-- Start info Area -->
+<section class="info-area relative section-gap" id="customer-service">
+    <div class="overlay overlay-bg"></div>
+    <div class="container">
+        <div class="row justify-content-start">
+            <div class="col-lg-5">
+                <h1 class="text-white">Dapatkan Pengalaman<br>
+                    Pelayanan Pelanggan<br>
+                    Terbaik Dari Kami</h1>
+                <p class="pt-20 pb-20">
+                    Hubungi Customer Service kami sekarang juga untuk konsultasi
+                    gratis - Diskon khusus untuk
+                    pelanggan yang beruntung.
+                </p>
+                <a href="https://api.whatsapp.com/send?phone=08xxx&amp;text=HALO" target="_blank" class="primary-btn header-btn text-uppercase">Hubungi Customer Service</a>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End info Area -->
+
+<section class="service-area section-gap d-none">
+    <div id="map"></div>
+</section>
+<!-- start footer Area -->
+<footer class="footer-area section-gap">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-3 col-lg-6 col-sm-12">
+                <div class="single-footer-widget text-xl-left text-center">
+                    <span class="widget-title">Head Office</span>
+                    <p>Jl. Mayjen Wiyono No. 1 Kota Malang  <br>
+                        <a href="tel:+08xxx" style="color:#3898f8;"><span class="lnr lnr-phone-handset"></span>&nbsp;<span class="text">08xxxxxx</span></a> <br>
+                        <a href="mailto:info@kursusmengemudi.test" style="color:#3898f8;"><span class="lnr lnr-envelope"></span>&nbsp;<span class="text">info@kursusmengemudi.test</span></a></p>
+                </div>
+            </div>
+            <div class="col-xl-auto col-lg-6 col-sm-12">
+                <div class="single-footer-widget text-sm-left text-center">
+                    <span class="widget-title">Alamat Cabang Lainnya</span>
+                    <ul class="footer-nav">
+                        <li>
+                            Benhil: Jl. Bendungan Hilir Raya No. 4 (Depan RS. Mintoharjo TNI-AL)
+                        </li>
+                        <li>
+                            Radio Dalam: Jl. Radio Dalam No. 37 (Depan Supermaket Grand Lucky)
+                        </li>
+                        <li>
+                            Gandaria: Jl. Gandaria I (Samping POM Bensin Pertamina)
+                        </li>
+                        <li>
+                            Percetakan Negara: Jl. Percetakan Negara Raya No. 46 D (Depan Mitra 10)
+                        </li>
+                        <li>
+                            Kramat Sentiong: Jl. Kramat Sentiong No. 27 (Belakang Nasgor Reegan)
+                        </li>
+                        <li>
+                        </li>Johar Baru: Jl. Pangkalan Asem Raya No. 6E (Depan Rajawali Elektronik)
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer-bottom d-flex justify-content-between align-items-center flex-wrap">
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            <p class="col-lg-8 col-sm-12 footer-text m-0 text-white">Copyright © <script>document.write(new Date().getFullYear());</script>2023 All rights reserved | Made with <i class="fa fa-heart-o" aria-hidden="true"></i> by Bagas Dicko Alfahmi</p>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            <div class="footer-social d-flex align-items-center">
+                <a href="tel:+08xxxx"><i class="fa fa-phone"></i></a>
+                <a href="mailto:
+                @gmail.com"><i class="fa fa-envelope"></i></a>
+                <a href="https://api.whatsapp.com/send?phone=08xxx&amp;text=Halo"><i class="fa fa-whatsapp"></i></a>
+                <a href="/"><i class="fa fa-globe"></i></a>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- End footer Area -->
+
+<script src="{{ asset('js/jquery-2.js')}}"></script>
+
+
+<nav id="mobile-nav">
+    <ul class="" style="touch-action: pan-y;" id="">
+        <li class="menu-active"><a href="/#beranda">Beranda</a></li>
+        <li><a href="/#jenis-kursus">Produk Terbaik</a></li>
+        <li><a href="/#tentang-kami">Tentang Kami</a></li>
+        <li><a href="/#kelebihan-kami">Kelebihan Kami</a></li>
+        <li><a href="/#customer-service">Hubungi Kami</a></li>
+    </ul>
+</nav><div id="mobile-body-overly"></div>
+
+</body>
+</html>
